a30d937e9a568b065c45d05b4bb029a7cb2d62cb
kauan f <kauanfelix10.a@gmail.com>
2023-11-30 00:39:37 -0300

    instalacão do handlebars e do nodemon

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express")
+const exphbs = require("express-handlebars")
 
 const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
 
 app.get('/', (requisicao, resposta) => {
     resposta.send("Olá, Mundo")
@@ -9,5 +13,3 @@ app.get('/', (requisicao, resposta) => {
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000")
 })
-
-const
